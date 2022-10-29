a0eace1eef8019f49bc7ac5e58a8eafb41a7980f
july-12 <seventanyb@gmail.com>
2022-10-29 15:52:07 +0800

    fix: no css file after build

--- packages/layout/tsdx.config.js
+++ packages/layout/tsdx.config.js
@@ -1,9 +1,23 @@
-const css = require('rollup-plugin-import-css');
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
   // This function will run for each entry/format/env combination
   rollup(config, options) {
-    config.plugins.push(css());
+    config.plugins.push(
+      postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            preset: 'default',
+          }),
+        ],
+        inject: false,
+        // only write out CSS for the first bundle (avoids pointless extra files):
+        extract: !!options.writeMeta,
+      })
+    );
     return config; // always return a config.
   },
 };
